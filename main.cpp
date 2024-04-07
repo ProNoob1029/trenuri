a25cfc656bd2347b3dd403abb6af337ee75b71da
ProNoob1029 <85897880+ProNoob1029@users.noreply.github.com>
2024-04-07 18:46:03 +0300

    reformat for trains

--- main.cpp
+++ main.cpp
@@ -1,55 +1,26 @@
 #include "iostream"
 #include "vector"
 #include "fstream"
+#include "string"
+#include "unordered_map"
 
-using std::vector, std::ifstream;
+using std::vector, std::ifstream, std::string, std::unordered_map;
 
 ifstream fin("input.in");
 
-struct Node;
-
-struct Edge {
-    int neighborIndex = -1;
-    int cost = 0;
+struct Railway {
+    string destination;
+    int distance;
 };
 
-struct Node {
-    int index = -1;
-    vector<Edge> neighbors;
-    int indexGraf = -1;
+struct Station {
+    string name;
+    vector<Railway> neighbours;
 };
 
-using Graph = vector<Node>;
+unordered_map<string, Station> stations;
 
-vector<Graph> grafuri;
-
-void read_file() {
-    int nodes, edges;
-    fin >> nodes >> edges;
-
-    Graph graf;
-
-    for (int i = 0; i < nodes; i++) {
-        Node node;
-        node.index = i;
-        node.indexGraf = grafuri.size();
-        graf.push_back(node);
-    }
-
-    for (int i = 0; i < edges; i++) {
-        int nod, vecin, cost;
-        fin >> nod >> vecin >> cost;
-        Edge edge;
-        edge.neighborIndex = vecin - 1;
-        edge.cost = cost;
-        graf[nod - 1].neighbors.push_back(edge);
-    }
-
-    grafuri.push_back(graf);
-}
 
 int main() {
-    printf("hi!\n");
-    read_file();
-    printf("ehe\n");
+
 }
